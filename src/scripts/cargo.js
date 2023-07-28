fa70535c3d93c1e73e28bf90a2075299220ace88
Nathan Chevalier <cerberusgfx@gmail.com>
2023-07-28 13:13:49 -0500

    Event listeners added to cargo ships

--- src/scripts/cargo.js
+++ src/scripts/cargo.js
@@ -1,15 +1,33 @@
-import { getCargo } from "./database.js";
+import { getCargo, getHaulers } from "./database.js";
+const cargoShips = getCargo();
+const haulers = getHaulers();
 
 export const cargoList = () => {
-  const cargoShips = getCargo();
-
   let cargoHTML = "<ul>";
 
   for (const ship of cargoShips) {
-    cargoHTML += `<li>${ship.shipName}</li>`;
+    cargoHTML += `<li data-haulerid="${ship.haulerId}" data-type="cargoShip" data-name="${ship.shipName}">${ship.shipName}</li>`;
   }
 
   cargoHTML += "</ul>";
 
   return cargoHTML;
 };
+
+document.addEventListener("click", (clickEvent) => {
+  const itemClicked = clickEvent.target;
+
+  // Was a shipping ship list item clicked?
+  if (itemClicked.dataset.type === "cargoShip") {
+    let haulerId = itemClicked.dataset.haulerid;
+    let haulingShip = { name: "Incorrect" };
+    for (const ship of haulers) {
+      if (ship.id === parseInt(haulerId)) {
+        haulingShip = { name: `${ship.shipName}` };
+        window.alert(
+          `${itemClicked.dataset.name} is being hauled by ${haulingShip.name}`
+        );
+      }
+    }
+  }
+});
