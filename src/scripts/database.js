f1a2a38d31926fca421cf810d9408a0b37f7b522
Nathan Chevalier <cerberusgfx@gmail.com>
2023-07-28 10:53:28 -0500

    Cargo ships outputting to HTML, state added to haulers

--- src/scripts/database.js
+++ src/scripts/database.js
@@ -12,6 +12,18 @@ const database = {
     { id: 4, shipName: "Tame Imphauler", dockId: 2 },
     { id: 5, shipName: "Slick Idiot", dockId: 1 },
   ],
+  cargoShips: [
+    { id: 1, shipName: "Amoco Cadiz", haulerId: 1 },
+    { id: 2, shipName: "Atlantic Empress", haulerId: 2 },
+    { id: 3, shipName: "Castillo de Bellver", haulerId: 3 },
+    { id: 4, shipName: "Odyssey", haulerId: 4 },
+    { id: 5, shipName: "Exxon Valdez", haulerId: 5 },
+    { id: 6, shipName: "ABT Summer", haulerId: 1 },
+    { id: 7, shipName: "Sea Empress", haulerId: 2 },
+    { id: 8, shipName: "Erika", haulerId: 3 },
+    { id: 8, shipName: "Rena", haulerId: 4 },
+    { id: 8, shipName: "Sanchi", haulerId: 5 },
+  ],
 };
 
 export const getDocks = () => {
@@ -20,4 +32,8 @@ export const getDocks = () => {
 
 export const getHaulers = () => {
   return database.haulers.map((haulers) => ({ ...haulers }));
+};
+
+export const getCargo = () => {
+  return database.cargoShips.map((cargoShips) => ({ ...cargoShips }));
 };
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,15 @@
 import { dockList } from "./docks.js";
 import { haulerList } from "./hauler.js";
+import { cargoList } from "./cargo.js";
 
 let dockDisplay = dockList();
 let haulerDisplay = haulerList();
+let cargoDisplay = cargoList();
 
 const dockHTML = document.querySelector("#dockList");
 const haulerHTML = document.querySelector("#haulerList");
+const cargoHTML = document.querySelector("#cargoList");
 
 dockHTML.innerHTML = dockDisplay;
 haulerHTML.innerHTML = haulerDisplay;
+cargoHTML.innerHTML = cargoDisplay;
